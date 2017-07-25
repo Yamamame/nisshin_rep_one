43cf4741ad3587bc2d24c470c52f1935432754fd
n.koyama <koyama@alion88.com>
2017-07-25 16:28:18 +0900

    [fix]とりあえず動くっていうレベルのところまで。

--- source/web/runExec.py
+++ source/web/runExec.py
@@ -1,4 +1,26 @@
-#!/usr/bin/python
-import numpy
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import subprocess
+import re
+import pprint
 
-print "hello world!!"
+args   = sys.argv
+cmd    = ''
+option = ''
+try:
+    cmd  = args[1]
+except IndexError:
+    print "not process!!"
+
+#arg1が無ければ終了
+if not cmd :
+	sys.exit()
+
+sprited = re.split('\s+',cmd,0)
+#pprint.pprint(sprited)
+cmd    = sprited[0]
+option =  sprited[1]
+print "start!!"
+check = subprocess.call(sprited)
+#print check
